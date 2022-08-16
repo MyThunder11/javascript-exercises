3dab9ec7082ab6e88da8f19b995c2a59a579a3fe
MyThunder11 <aminebenani44@gmail.com>
2022-08-16 22:43:41 +0200

    exercise 7 done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(fTemp) {
+    let newCTemp = (fTemp - 32) * (5/9)
+    return +(Math.round(newCTemp + "e+1")  + "e-1");
 };
 
-const ctof = function() {
+const ctof = function(cTemp) {
+    let newFTemp = (cTemp * (9/5)) + 32;
+    return +(Math.round(newFTemp + "e+1")  + "e-1");
 
 };
 
