00c7deacbbbb03da60dce4a0849577b7d25fc5b1
MyThunder11 <aminebenani44@gmail.com>
2022-08-16 22:07:58 +0200

    Exercise 5 done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,15 @@
-const sumAll = function() {
+const sumAll = function(...args) {
+    if (args[0] < 0 || args[1] < 0) {
+        return 'ERROR'
+    } 
+    if (!Number.isInteger(args[0]) || !Number.isInteger(args[1])) {
+        return 'ERROR'
+    }
+    max = Math.max(args[0], args[1]);
+    min = Math.min(args[0], args[1]);
+    let sum = (max * (max + 1) / 2) - ((min - 1) * min / 2);
+    return sum
+
 
 };
 
