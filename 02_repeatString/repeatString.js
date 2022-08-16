3b9dd5f4e753b211ffee2014f698334e846aee96
MyThunder11 <aminebenani44@gmail.com>
2022-08-16 12:14:57 +0200

    Exercise 2 done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
-
-};
+const repeatString = function(string, num) {
+        if (num === 0) {
+            const empty = ""
+            return empty;
+        } else if (num < 0) {
+            const error = 'ERROR';
+            return error;
+        }
+        let str = string.repeat(num);
+        return str
+    }
 
 // Do not edit below this line
 module.exports = repeatString;
