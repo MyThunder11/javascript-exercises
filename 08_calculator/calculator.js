9c68056ddb0bc49ef78b2d008e6624086dacc071
MyThunder11 <aminebenani44@gmail.com>
2022-08-26 14:13:37 +0200

    exercise 8 done

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a - b
 };
 
-const sum = function() {
-	
+const sum = function([...a]) {
+	const sum = a.reduce((total, number) => {return total += number}, 0);
+  return +sum;
 };
 
-const multiply = function() {
-
+const multiply = function([...a]) {
+  const multiply = a.reduce((total, number) => total *= number, 1);
+  return +multiply;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let total = 1;
+  for(let i = a; i > 0; i--) {
+    total *= i;
+  }
+  return total;
 };
 
 // Do not edit below this line
